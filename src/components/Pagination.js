56d1f0e8c9bf938d5dcd62eb05946a3b383f2aa8
ossman rauf <ossmanrauf8@gmail.com>
2021-04-03 20:44:49 +0200

    sintax error resolved

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,46 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 const Pagination = ({ cardsPerPage, totalCards, changePage, currentPage }) => {
-    const pageNumbers = [];
-    console.log("im at pagination");
-    console.log(Math.ceil(totalCards / cardsPerPage), totalCards);
-    for (let i = 1; i <= Math.ceil(totalCards / cardsPerPage); i++) {
-        console.log(i);
-        pageNumbers.push(i);
-    }
-    return (
-        // <div className="pagination">
-        <
-        nav aria - label = "..."
-        className = "pagination-nav" >
-        <
-        ul className = "pagination" > {
-            pageNumbers.map((number) => {
-                console.log(number, "hh");
-                return ( <
-                    li key = { number }
-                    className = "page-item"
-                    // if the page active is the same as the current page disable some styles
-                    style = { currentPage === number ? activated : {} } >
-                    <
-                    a onClick = {
-                        () => changePage(number) }
-                    className = "page-link"
-                    href = "/" > { number } <
-                    /a> <
-                    /li>
-                );
-            })
-        } <
-        /ul> <
-        /nav>
-        // </div>
-    );
+	const pageNumbers = [];
+	console.log("im at pagination");
+	console.log(Math.ceil(totalCards / cardsPerPage), totalCards);
+	for (let i = 1; i <= Math.ceil(totalCards / cardsPerPage); i++) {
+		console.log(i);
+		pageNumbers.push(i);
+	}
+	return (
+		// <div className="pagination">
+		<nav aria-label="..." className="pagination-nav">
+			<ul className="pagination">
+				{" "}
+				{pageNumbers.map((number) => {
+					console.log(number, "hh");
+					return (
+						<li
+							key={number}
+							className="page-item"
+							// if the page active is the same as the current page disable some styles
+							style={currentPage === number ? activated : {}}>
+							<a
+								onClick={() => changePage(number)}
+								className="page-link"
+								href="/">
+								{" "}
+								{number}{" "}
+							</a>{" "}
+						</li>
+					);
+				})}{" "}
+			</ul>{" "}
+		</nav>
+		// </div>
+	);
 };
 // style for active page
 const activated = {
-    cursor: "default",
-    pointerEvents: "none",
-    textDecoration: "none",
+	cursor: "default",
+	pointerEvents: "none",
+	textDecoration: "none",
 };
 
 export default Pagination;
